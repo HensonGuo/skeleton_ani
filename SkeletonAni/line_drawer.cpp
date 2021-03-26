815ae90d2be30ee9fbce45fa116fa7a53c1390ed
郭昀霖 <1851228745@qq.com>
2021-03-26 15:54:32 +0800

    内存泄漏问题

--- SkeletonAni/line_drawer.cpp
+++ SkeletonAni/line_drawer.cpp
@@ -3,20 +3,36 @@
 
 LineDrawer::LineDrawer()
 {
+	vertices = new std::vector<Vertex>();
+
+	glLineWidth(lineWidth);
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, position));
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, color));
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 1, GL_UNSIGNED_INT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, index));
+	glBindVertexArray(0);
 }
 
-void LineDrawer::addBoneLine(vec3 start, uint startBoneId, vec3 end, uint endBoneId, vec4 color)
+void LineDrawer::addBoneLine(vec3 start, uint startId, vec3 end, uint endId, vec4 color)
 {
 	Vertex startVertex;
 	startVertex.position = start;
-	startVertex.boneId = startBoneId;
+	startVertex.index = startId;
 	startVertex.color = color;
-	vertices.push_back(startVertex);
+	vertices->push_back(startVertex);
 	Vertex endVertex;
 	endVertex.position = end;
-	endVertex.boneId = endBoneId;
+	endVertex.index = endId;
 	endVertex.color = color;
-	vertices.push_back(endVertex);
+	vertices->push_back(endVertex);
 }
 
 void LineDrawer::draw(Shader& shader)
@@ -24,26 +40,10 @@ void LineDrawer::draw(Shader& shader)
 	glBindVertexArray(VAO);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glDrawArrays(GL_LINES, 0, vertices.size());
-	glDisable(GL_BLEND);
-}
-
-void LineDrawer::setUp()
-{
-	glLineWidth(lineWidth);
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.size(), &vertices[0], GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, position));
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, color));
-	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(2, 1, GL_UNSIGNED_INT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, boneId));
-	glBindVertexArray(0);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices->size(), &(*vertices)[0], GL_STATIC_DRAW);
+	glDrawArrays(GL_LINES, 0, vertices->size());
+	glDisable(GL_BLEND);
 }
 
 void LineDrawer::setLineWidth(float lineWidth)
@@ -54,5 +54,7 @@ void LineDrawer::setLineWidth(float lineWidth)
 
 void LineDrawer::clear()
 {
-	vertices.clear();
+	vertices->clear();
+	delete vertices;
+	vertices = new std::vector<Vertex>();
 }
--- SkeletonAni/line_drawer.h
+++ SkeletonAni/line_drawer.h
@@ -10,20 +10,19 @@ using namespace glm;
 class LineDrawer {
 public:
 	LineDrawer();
-	void addBoneLine(vec3 start, uint startBoneId, vec3 end, uint endBoneId, vec4 color);
+	void addBoneLine(vec3 start, uint startId, vec3 end, uint endId, vec4 color);
 	void draw(Shader& shader);
-	void setUp();
 	void setLineWidth(float lineWidth);
 	void clear();
 private:
 	struct Vertex {
 		vec3 position;
 		vec4 color;
-		uint boneId;
+		int index;
 	};
 	GLuint VAO;
 	GLuint VBO;
-	std::vector<Vertex> vertices;
+	std::vector<Vertex> *vertices;
 	float lineWidth = 1.0f;
 };
 
